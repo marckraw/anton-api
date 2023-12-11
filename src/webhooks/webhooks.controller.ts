9680980359e700a0071b003a6e175a7e922e0d2b
marckraw <marckraw@icloud.com>
2023-12-11 10:53:55 +0100

    add sample

--- src/webhooks/webhooks.controller.ts
+++ src/webhooks/webhooks.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 
 @Controller('webhooks')
-export class WebhooksController {}
+export class WebhooksController {
+  @Post('catch')
+  catchWebhook(@Body() payload: any) {
+    console.log('This is payload: ');
+    console.log(payload);
+    return payload;
+  }
+}
