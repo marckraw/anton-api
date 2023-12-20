4a981f7f23eb086d1d6233c76e9e819cb9a6a035
marckraw <marckraw@icloud.com>
2023-12-20 23:20:30 +0100

    just trigger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+console.log("Just trigger console.log");
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
