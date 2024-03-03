8366680e1511000179de2df7a6e7e3c935f19dae
marckraw <marckraw@icloud.com>
2024-03-03 14:52:19 +0100

    feat: changes for the whole move to postgres and digital ocean

--- knexfile.ts
+++ knexfile.ts
@@ -1,12 +1,27 @@
 import type { Knex } from 'knex';
 import { knexSnakeCaseMappers } from 'objection';
 
+console.log("Knexfile")
+console.log(process.env.POSTGRES_HOST);
+console.log(process.env.POSTGRES_USER);
+
+// Assuming these values are being correctly pulled from your .env file
+const POSTGRES_HOST = process.env.POSTGRES_HOST; // This should be 'db' in Docker context
+const POSTGRES_USER = process.env.POSTGRES_USER;
+const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD;
+const POSTGRES_DB = process.env.POSTGRES_DB;
+const POSTGRES_PORT = process.env.POSTGRES_PORT; // Default to 5432 if not specified
+
 const config: { [key: string]: Knex.Config } = {
   development: {
-    client: 'better-sqlite3',
+    client: 'pg',
     useNullAsDefault: true,
     connection: {
-      filename: './src/database/anton-db.sqlite3',
+      host: POSTGRES_HOST,
+      user: POSTGRES_USER,
+      password: POSTGRES_PASSWORD,
+      database: POSTGRES_DB,
+      port: Number(POSTGRES_PORT), // Default PostgreSQL port
     },
     migrations: {
       tableName: 'migrations',
--- src/main.ts
+++ src/main.ts
@@ -47,7 +47,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(30152);
+  await app.listen(3000);
 }
 
 console.log('lkajsldkjalskdjl');
